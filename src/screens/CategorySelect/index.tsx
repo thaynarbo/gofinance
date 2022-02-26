f503d38bb46ee2260406411b40980ff621a25219
thaynarbo <thaynarbo@gmail.com>
2022-02-25 21:45:08 -0300

    feat: Add screen to select category

---
+++ src/screens/CategorySelect/index.tsx
@@ -0,0 +1,61 @@
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+import {
+    Container,
+    Header,
+    Title,
+    Category,
+    Icon,
+    Name,
+    Separator,
+    Footer,
+} from "./styles";
+
+import { Button } from "../../components/Form/Button";
+import { categories } from "../../utils/categories";
+
+interface Category {
+    key: string;
+    name: string;
+}
+
+interface Props {
+    category: Category;
+    setCategory: (category: Category) => void;
+    closeSelectCategory: () => void;
+}
+
+export function CategorySelect({
+    category,
+    setCategory,
+    closeSelectCategory,
+}: Props) {
+    function handleCategorySelect(category: Category) {
+        setCategory(category);
+    }
+    return (
+        <Container>
+            <Header>
+                <Title>Categoria</Title>
+            </Header>
+            <FlatList
+                data={categories}
+                style={{ flex: 1, width: "100%" }}
+                keyExtractor={(item) => item.key}
+                renderItem={({ item }) => (
+                    <Category
+                        onPress={() => handleCategorySelect(item)}
+                        isActive={category.key === item.key}
+                    >
+                        <Icon name={item.icon} />
+                        <Name>{item.name}</Name>
+                    </Category>
+                )}
+                ItemSeparatorComponent={() => <Separator />}
+            />
+            <Footer>
+                <Button title="selecionar" onPress={closeSelectCategory} />
+            </Footer>
+        </Container>
+    );
+}
