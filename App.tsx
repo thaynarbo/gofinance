638a6c35496ce3c27ba10967f04c12cfab589d3e
Thaynar Lima <thaynar.lima@db1.com.br>
2022-03-08 22:12:29 -0300

    feat: add light statusBar style

--- App.tsx
+++ App.tsx
@@ -5,10 +5,10 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 
 import {
-    Poppins_400Regular,
-    Poppins_500Medium,
-    Poppins_700Bold,
-    useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  useFonts,
 } from "@expo-google-fonts/poppins";
 
 import AppLoading from "expo-app-loading";
@@ -18,22 +18,24 @@ import theme from "./src/global/styles/theme";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AppRoutes } from "./src/routes/app.routes";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-    const [fontsLoaded] = useFonts({
-        Poppins_400Regular,
-        Poppins_500Medium,
-        Poppins_700Bold,
-    });
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
 
-    if (!fontsLoaded) {
-        return <AppLoading />;
-    }
-    return (
-        <ThemeProvider theme={theme}>
-            <NavigationContainer>
-                <AppRoutes />
-            </NavigationContainer>
-        </ThemeProvider>
-    );
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <StatusBar style={"light"} />
+        <AppRoutes />
+      </NavigationContainer>
+    </ThemeProvider>
+  );
 }
