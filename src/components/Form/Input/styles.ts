9a9af4b6a1cc1fc1a46d15d6dd7782be94220ac2
Thaynar Lima <thaynar.lima@db1.com.br>
2022-03-08 22:17:49 -0300

    ios build

--- src/components/Form/Input/styles.ts
+++ src/components/Form/Input/styles.ts
@@ -3,15 +3,15 @@ import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.TextInput`
-    width: 100%;
-    padding: 16px 18px;
+  width: 100%;
+  padding: 16px 18px;
 
-    font-family: ${({ theme }) => theme.font.regular};
-    font-size: ${RFValue(14)}px;
-    color: ${({ theme }) => theme.colors.text_dark};
+  font-family: ${({ theme }) => theme.font.regular};
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.text_dark};
 
-    background-color: ${({ theme }) => theme.colors.shape};
-    border-radius: 5px;
+  background-color: ${({ theme }) => theme.colors.shape};
+  border-radius: 5px;
 
-    margin-bottom: 8px;
+  margin-bottom: 8px;
 `;
